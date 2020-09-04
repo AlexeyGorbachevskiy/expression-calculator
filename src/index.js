1053e468527f466cd7953349370f6946d56b05df
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-09-04 14:54:53 +0300

    feat: implement expression-calculator task

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+
+    if(expr.includes('/ 0')||expr.includes('/0')){
+        throw new Error('TypeError: Division by zero.')
+    }
+
+    let count=0;
+    for(let el of expr){
+        if(el==='('){
+            count++
+        }
+        if(el===')'){
+            count--
+        }
+    }
+    if(count!==0){
+        throw new Error('ExpressionError: Brackets must be paired')
+    }
+
+    return  new Function(`return ${expr}`)()
 }
 
 module.exports = {
